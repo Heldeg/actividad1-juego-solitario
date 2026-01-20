7003279705389ed795c664e752d14a7561399bd6
Edder H F <edhernandezfo@unal.edu.co>
2026-01-19 20:02:00 -0500

    refactor: change names to english

--- js/solitario.js
+++ js/solitario.js
@@ -1,44 +1,47 @@
 /***** INICIO DECLARACIÓN DE VARIABLES GLOBALES *****/
 
 // Array de palos
-let palos = ["viu", "cua", "hex", "cir"];
+let suits = ["viu", "cua", "hex", "cir"];
 // Array de número de cartas
-//let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//let numberRange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // En las pruebas iniciales solo se trabajará con cuatro cartas por palo:
-let numeros = [9, 10, 11, 12];
+
+//TODO: allow the user to decide the range in the UI
+let numberRange = [9, 10, 11, 12];
+
 
 // paso (top y left) en pixeles de una carta a la siguiente en un mazo
 let paso = 5;
 
 // Tapetes				
-let tapeteInicial   = document.getElementById("inicial");
-let tapeteSobrantes = document.getElementById("sobrantes");
-let tapeteReceptor1 = document.getElementById("receptor1");
-let tapeteReceptor2 = document.getElementById("receptor2");
-let tapeteReceptor3 = document.getElementById("receptor3");
-let tapeteReceptor4 = document.getElementById("receptor4");
+let initMat   = document.getElementById("inicial");
+let leftoverCardMat = document.getElementById("sobrantes");
+let receptorMat1 = document.getElementById("receptor1");
+let receptorMat2 = document.getElementById("receptor2");
+let receptorMat3 = document.getElementById("receptor3");
+let receptorMat4 = document.getElementById("receptor4");
 
 // Mazos
-let mazoInicial   = [];
-let mazoSobrantes = [];
-let mazoReceptor1 = [];
-let mazoReceptor2 = [];
-let mazoReceptor3 = [];
-let mazoReceptor4 = [];
+let initDeck   = [];
+let leftoverDeck = [];
+let receptorDeck1 = [];
+let receptorDeck2 = [];
+let receptorDeck3 = [];
+let receptorDeck4 = [];
 
 // Contadores de cartas
-let contInicial     = document.getElementById("contador_inicial");
-let contSobrantes   = document.getElementById("contador_sobrantes");
-let contReceptor1   = document.getElementById("contador_receptor1");
-let contReceptor2   = document.getElementById("contador_receptor2");
-let contReceptor3   = document.getElementById("contador_receptor3");
-let contReceptor4   = document.getElementById("contador_receptor4");
-let contMovimientos = document.getElementById("contador_movimientos");
+let initCount     = document.getElementById("contador_inicial");
+let leftoverCount   = document.getElementById("contador_sobrantes");
+let receptorCount1   = document.getElementById("contador_receptor1");
+let receptorCount2   = document.getElementById("contador_receptor2");
+let receptorCount3   = document.getElementById("contador_receptor3");
+let receptorCount4   = document.getElementById("contador_receptor4");
+let moveCount = document.getElementById("contador_movimientos");
 
 // Tiempo
-let contTiempo  = document.getElementById("contador_tiempo"); // span cuenta tiempo
-let segundos 	 = 0;    // cuenta de segundos
-let temporizador = null; // manejador del temporizador
+let timerCount  = document.getElementById("contador_tiempo"); // span cuenta tiempo
+let seconds 	 = 0;    // cuenta de segundos
+let timer = null; // manejador del temporizador
 
 /***** FIN DECLARACIÓN DE VARIABLES GLOBALES *****/
 
@@ -51,7 +54,7 @@ let temporizador = null; // manejador del temporizador
 /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
 
 // Desarrollo del comienzo de juego
-function comenzarJuego() {
+function startGame() {
 	/* Crear baraja, es decir crear el mazoInicial. Este será un array cuyos 
 	elementos serán elementos HTML <img>, siendo cada uno de ellos una carta.
 	Sugerencia: en dos bucles for, bárranse los "palos" y los "numeros", formando
@@ -99,22 +102,22 @@ function comenzarJuego() {
 	a clearInterval en su caso.   
 */
 
-function arrancarTiempo(){
+function setTimer(){
 	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-	if (temporizador) clearInterval(temporizador);
+	if (timer) clearInterval(timer);
     let hms = function (){
-			let seg = Math.trunc( segundos % 60 );
-			let min = Math.trunc( (segundos % 3600) / 60 );
-			let hor = Math.trunc( (segundos % 86400) / 3600 );
+			let seg = Math.trunc( seconds % 60 );
+			let min = Math.trunc( (seconds % 3600) / 60 );
+			let hor = Math.trunc( (seconds % 86400) / 3600 );
 			let tiempo = ( (hor<10)? "0"+hor : ""+hor ) 
 						+ ":" + ( (min<10)? "0"+min : ""+min )  
 						+ ":" + ( (seg<10)? "0"+seg : ""+seg );
-			setContador(contTiempo, tiempo);
-            segundos++;
+			setCounter(timerCount, tiempo);
+            seconds++;
 		}
-	segundos = 0;
+	seconds = 0;
     hms(); // Primera visualización 00:00:00
-	temporizador = setInterval(hms, 1000);
+	timer = setInterval(hms, 1000);
     	
 } // arrancarTiempo
 
@@ -125,7 +128,7 @@ function arrancarTiempo(){
 	por referencia, de modo que si se altera el orden de dicho array
 	dentro de la rutina, esto aparecerá reflejado fuera de la misma.
 */
-function barajar(mazo) {
+function shuffle(mazo) {
 	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/	
 } // barajar
 
@@ -138,7 +141,7 @@ function barajar(mazo) {
 	coordenadas top y left, algun atributo de tipo data-...
 	Al final se debe ajustar el contador de cartas a la cantidad oportuna
 */
-function cargarTapeteInicial(mazo) {
+function setupInitMat(mazo) {
 	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/	
 } // cargarTapeteInicial
 
@@ -147,21 +150,21 @@ function cargarTapeteInicial(mazo) {
  	Esta función debe incrementar el número correspondiente al contenido textual
    	del elemento que actúa de contador
 */
-function incContador(contador){
+function startCounter(contador){
 	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/	
 } // incContador
 
 /**
 	Idem que anterior, pero decrementando 
 */
-function decContador(contador){
+function decCounter(contador){
 	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! ***/	
-} // decContador
+} // decCounter
 
 /**
 	Similar a las anteriores, pero ajustando la cuenta al
 	valor especificado
 */
-function setContador(contador, valor) {
+function setCounter(contador, valor) {
 	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
 } // setContador
